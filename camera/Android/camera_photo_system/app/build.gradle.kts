69b58c84294b9f72479c48414a07ba54c517281e
guhuan7 <769540542@qq.com>
2025-03-21 19:18:09 -0700

    123

--- camera/Android/camera_photo_system/app/build.gradle.kts
+++ camera/Android/camera_photo_system/app/build.gradle.kts
@@ -47,9 +47,9 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
-    packaging {
+    packagingOptions {
         resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes.add("/META-INF/{AL2.0,LGPL2.1}")
         }
     }
 }
